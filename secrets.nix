67329b4bae24b855fd162fbc8f8c198cdf6f2e0b
Bruno De Masi <bruno@demasi.dev>
2026-03-21 20:38:35 -0300

    Add whatsappAllowedSenders secret

--- secrets.nix
+++ secrets.nix
@@ -24,6 +24,9 @@ in
   # User password
   "hashedUserPassword.age".publicKeys = all;
 
+  # WhatsApp
+  "whatsappAllowedSenders.age".publicKeys = all;
+
   # OpenFang
   "openfangApiKey.age".publicKeys = all;
 
